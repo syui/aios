29c47fbe9ca0469fedf4c95bbe33754fbcf516c5
syui <syui@syui.ai>
2026-02-27 14:45:55 +0900

    fix cfg

--- cfg/pkg.sh
+++ cfg/pkg.sh
@@ -4,10 +4,14 @@ set -e
 
 ROOTFS="$1"
 
+arch-chroot $ROOTFS /bin/sh -c 'rm -rf /tmp/gpt /tmp/log /tmp/shell'
 arch-chroot $ROOTFS /bin/sh -c '
-cd /tmp
-git clone https://git.syui.ai/ai/gpt && cd gpt && cargo build --release && cp target/release/aigpt /usr/local/bin/ && cd ..
-git clone https://git.syui.ai/ai/log && cd log && cargo build --release && cp target/release/ailog /usr/local/bin/ && cd ..
-git clone https://git.syui.ai/ai/shell && cd shell && cargo build --release && cp target/release/aishell /usr/local/bin/ && cd ..
-rm -rf gpt log shell
+cd /tmp && git clone https://git.syui.ai/ai/gpt && cd /tmp/gpt && cargo build --release && cp target/release/aigpt /usr/local/bin/
 '
+arch-chroot $ROOTFS /bin/sh -c '
+cd /tmp && git clone -b main https://git.syui.ai/ai/log && cd /tmp/log && cargo build --release && cp target/release/ailog /usr/local/bin/
+'
+arch-chroot $ROOTFS /bin/sh -c '
+cd /tmp && git clone https://git.syui.ai/ai/shell && cd /tmp/shell && cargo build --release && cp target/release/aishell /usr/local/bin/
+'
+arch-chroot $ROOTFS /bin/sh -c 'rm -rf /tmp/gpt /tmp/log /tmp/shell'
